b89e053d322795ad5067d9297b3a2e5a798629ec
otaviootavio <otaviovacari@usp.br>
2023-12-19 21:11:58 -0300

    create routes

--- src/index.ts
+++ src/index.ts
@@ -7,128 +7,25 @@ import {
   getRoom,
   getRooms,
 } from "./controllers/roomController";
-import { RoomModel } from "./models/RoomModel";
-import { UserAlreadyExistsOnRoom } from "./libs/RoomErrors";
-import { NotFoundError } from "elysia";
 import { tRoomModel } from "./models/room.model";
 import { tRoomsModel } from "./models/rooms.model";
+import {
+  addUserToRoomRoute,
+  createRoomRoute,
+  deleteRoomRoute,
+  getRoomByIdRoute,
+  getRoomRoute,
+} from "./route";
 
 const app = new Elysia()
-  .use(swagger())
   .use(tRoomsModel)
   .use(tRoomModel)
-  .onError(({ code, error, set }) => {
-    if (code === "NOT_FOUND") {
-      set.status = 404;
-
-      return error.message;
-    }
-  })
-  .post(
-    "/room",
-    (): RoomModel => {
-      return createRoom();
-    },
-    {
-      afterHandle: ({ set }) => {
-        set.status = "Created";
-      },
-      response: {
-        201: "room",
-      },
-    }
-  )
-  .get(
-    "/rooms",
-    (): RoomModel[] => {
-      return getRooms();
-    },
-    {
-      afterHandle: ({ set }) => {
-        set.status = "Created";
-      },
-      response: {
-        201: "rooms",
-      },
-    }
-  )
-  .onError(({ code, error, set }) => {
-    if (code === "NOT_FOUND") {
-      set.status = 404;
-      return error.message;
-    }
-  })
-  .get(
-    "/room/:roomId",
-    ({ params: { roomId } }) => {
-      return getRoom(roomId);
-    },
-    {
-      params: t.Object({
-        roomId: t.String(),
-      }),
-      response: {
-        200: "room",
-      },
-    }
-  )
-  .error({
-    UserAlreadyExistsOnRoom,
-    NotFoundError,
-  })
-  .onError(({ code, error, set }) => {
-    switch (code) {
-      case "UserAlreadyExistsOnRoom":
-        //Conflict
-        set.status = 409;
-        return error;
-      case "NotFoundError":
-        set.status = "Not Found";
-        return error;
-    }
-  })
-  .put(
-    "/room/:roomId/:user",
-    ({ params: { roomId, user } }) => {
-      addUserToRoom(roomId, user);
-    },
-    {
-      params: t.Object({
-        roomId: t.String(),
-        user: t.String(),
-      }),
-      afterHandle: ({ set }) => {
-        set.status = "Created";
-      },
-      response: {
-        201: t.Void(),
-      },
-    }
-  )
-  .onError(({ code, error, set }) => {
-    if (code === "NOT_FOUND") {
-      set.status = 404;
-
-      return error.message;
-    }
-  })
-  .delete(
-    "/room/:roomId",
-    ({ params: { roomId } }) => {
-      deleteRoom(roomId);
-    },
-    {
-      params: t.Object({
-        roomId: t.String(),
-      }),
-      afterHandle: ({ set }) => {
-        set.status = "Created";
-      },
-      response: {
-        201: t.Void(),
-      },
-    }
-  );
+  .use(swagger())
+  .use(createRoomRoute)
+  .use(getRoomRoute)
+  .use(getRoomByIdRoute)
+  .use(addUserToRoomRoute)
+  .use(deleteRoomRoute);
 
 app.listen(3000);
 console.log(
---
+++ src/route.ts
@@ -0,0 +1,143 @@
+import Elysia, { t } from "elysia";
+import { tRoomsModel } from "./models/rooms.model";
+import { tRoomModel } from "./models/room.model";
+import {
+  addUserToRoom,
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} from "./controllers/roomController";
+import { UserAlreadyExistsOnRoom } from "./libs/RoomErrors";
+import { NotFoundError } from "elysia";
+
+const createRoomRoute = new Elysia()
+  .use(tRoomsModel)
+  .use(tRoomModel)
+  .onError(({ code, error, set }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+
+      return error.message;
+    }
+  })
+  .post(
+    "/room",
+    () => {
+      return createRoom();
+    },
+    {
+      afterHandle: ({ set }) => {
+        set.status = "Created";
+      },
+      response: {
+        201: "room",
+      },
+    }
+  );
+
+const getRoomRoute = new Elysia().use(tRoomsModel).get(
+  "/rooms",
+  () => {
+    return getRooms();
+  },
+  {
+    afterHandle: ({ set }) => {
+      set.status = "Created";
+    },
+    response: {
+      201: "rooms",
+    },
+  }
+);
+
+const getRoomByIdRoute = new Elysia()
+  .use(tRoomModel)
+  .onError(({ code, error, set }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+      return error.message;
+    }
+  })
+  .get(
+    "/room/:roomId",
+    ({ params: { roomId } }) => {
+      return getRoom(roomId);
+    },
+    {
+      params: t.Object({
+        roomId: t.String(),
+      }),
+      response: {
+        200: "room",
+      },
+    }
+  );
+const addUserToRoomRoute = new Elysia()
+  .error({
+    UserAlreadyExistsOnRoom,
+    NotFoundError,
+  })
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case "UserAlreadyExistsOnRoom":
+        //Conflict
+        set.status = 409;
+        return error;
+      case "NotFoundError":
+        set.status = "Not Found";
+        return error;
+    }
+  })
+  .put(
+    "/room/:roomId/:user",
+    ({ params: { roomId, user } }) => {
+      addUserToRoom(roomId, user);
+    },
+    {
+      params: t.Object({
+        roomId: t.String(),
+        user: t.String(),
+      }),
+      afterHandle: ({ set }) => {
+        set.status = "Created";
+      },
+      response: {
+        201: t.Void(),
+      },
+    }
+  );
+
+const deleteRoomRoute = new Elysia()
+  .onError(({ code, error, set }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+
+      return error.message;
+    }
+  })
+  .delete(
+    "/room/:roomId",
+    ({ params: { roomId } }) => {
+      deleteRoom(roomId);
+    },
+    {
+      params: t.Object({
+        roomId: t.String(),
+      }),
+      afterHandle: ({ set }) => {
+        set.status = "Created";
+      },
+      response: {
+        201: t.Void(),
+      },
+    }
+  );
+
+export {
+  createRoomRoute,
+  getRoomRoute,
+  getRoomByIdRoute,
+  addUserToRoomRoute,
+  deleteRoomRoute,
+};
